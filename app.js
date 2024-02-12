91fba45a4a5f8cb1fc72657f43c2d1546ece259e
George Gordon <georgegordon@Georges-MBP.lan>
2024-02-12 16:24:42 +0000

    arrays of week and months added

--- app.js
+++ app.js
@@ -1,25 +1,56 @@
-const calender = document.getElementById('calender');
+const calender = document.getElementById("calender");
+
+const months = [ "January",
+ "February",
+  "March",
+   "April",
+    "May",
+     "June",
+      "July",
+       "August",
+        "September",
+         "October",
+          "November",
+           "December" ];
+
+           const days = ["Sunday",
+            "Monday",
+             "Tuesday",
+              "Wednesday",
+               "Thursday",
+                "Friday", 
+                "Saturday"];
 
 const drawBlankCalender = () => {
-    for (let i = 0; i < 35; i++) {
-    const day = document.createElement('div');
-    day.classList.add('day');
+  for (let i = 0; i < 35; i++) {
+    const day = document.createElement("div");
+    day.classList.add("day");
 
-    const dayText = document.createElement('p');
-    dayText.classList.add('day-text');
-  
+    const dayText = document.createElement("p");
+    dayText.classList.add("day-text");
 
-    const dayNumber = document.createElement('p');
-    dayNumber.classList.add('day-number');
+    const dayNumber = document.createElement("p");
+    dayNumber.classList.add("day-number");
 
-    const eventName = document.createElement('p');
-    eventName.classList.add('event-name');
+    const eventName = document.createElement("p");
+    eventName.classList.add("event-name");
 
     day.appendChild(dayText);
     day.appendChild(dayNumber);
     day.appendChild(eventName);
     console.log(day);
     calender.appendChild(day);
-}
+  }
 };
+const updateCalender = (month, year, events) => {
+const dayElements = document.querySelectorAll(".day");
+
+const theFirst = new Date();
+theFirst.setMonth(month);
+theFirst.setFullYear(year);
+
+const theFirstDayOfWeek = theFirst.getDay();
+const
+}
+
 drawBlankCalender();
